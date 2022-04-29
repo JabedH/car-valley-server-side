0d42fb2eb9f0ad6d4d5e9ec77c9c1f3edea41de8
JabedH <87475551+JabedH@users.noreply.github.com>
2022-04-29 22:53:11 +0600

    get data from db

--- index.js
+++ index.js
@@ -9,6 +9,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kd59j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const carCollection = client.db("CarValley").collection("Cars");
+    //
+    app.get("/Cars", async (req, res) => {
+      const query = {};
+      const cursor = carCollection.find(query);
+      const car = await cursor.toArray();
+      res.send(car);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("server is running");
 });
